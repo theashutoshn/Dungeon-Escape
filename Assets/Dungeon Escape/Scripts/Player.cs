d68ba0ca74892f9cce0af85fb1e8532c89e016aa
AshutoshNandanwar <95475646+theashutoshn@users.noreply.github.com>
2024-01-23 01:18:49 +0530

    Player Jump - I

--- Assets/Dungeon Escape/Scripts/Player.cs
+++ Assets/Dungeon Escape/Scripts/Player.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D _rb;
+    private Rigidbody2D _rb2d;
+    private float _jumpForce = 5f;
+    [SerializeField]
+    private bool _grounded = false;
+    [SerializeField]
+    private LayerMask _groundLayer;
+
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,6 +21,26 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        _rb.velocity = new Vector2(horizontalInput, _rb.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            _rb2d.velocity = new Vector2(_rb2d.velocity.x, _jumpForce);
+            _grounded = false;
+        }
+
+        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, _groundLayer.value);
+
+        Debug.DrawRay(transform.position, Vector2.down * 0.8f, Color.green);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+            _grounded = true;
+        }
+
+
+
+        _rb2d.velocity = new Vector2(horizontalInput, _rb2d.velocity.y);
+
     }
 }
