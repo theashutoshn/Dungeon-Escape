6d0d5cd75559273215e84a386e0bfaca6e6abaab
Ashutosh <ashunandanwar98@gmail.com>
2024-05-20 18:04:13 +0530

    Game Manager, Buy Method and Paint Diamonds

--- Assets/Dungeon Escape/Scripts/Shop/Shop.cs
+++ Assets/Dungeon Escape/Scripts/Shop/Shop.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public GameObject shopPanel;
+
+    //variable for currentItemSelected
+    public int currentSelectedItem;
+    public int currentItemCost;
+
+    private Player _player;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            Player player = other.GetComponent<Player>();
+            _player = other.GetComponent<Player>();
 
-            if(player != null)
+            if(_player != null)
             {
-                UIManager.Instance.OpenShop(player.diamond);
+                UIManager.Instance.OpenShop(_player.diamond);
             }
             
 
@@ -38,17 +44,51 @@ public class Shop : MonoBehaviour
         {
             case 0:
                 UIManager.Instance.UPdateShopSelection(106);
+                currentSelectedItem = 0;
+                currentItemCost = 200;
                 break;
 
             case 1:
                 UIManager.Instance.UPdateShopSelection(6);
+                currentSelectedItem = 1;
+                currentItemCost = 400;
                 break;
 
             case 2:
                 UIManager.Instance.UPdateShopSelection(-106);
+                currentSelectedItem = 2;
+                currentItemCost = 100;
                 break;
 
 
         }
     }
+
+    // Buy method
+
+    public void BuyItemm()
+    {
+        if(_player.diamond >= currentItemCost)
+        {
+            if(currentSelectedItem == 2)
+            {
+                GameManager.Instance.HasKeyToCastle = true;
+            }
+
+            _player.diamond -= currentItemCost;
+            Debug.Log("AwardItem");
+            Debug.Log("Purchased" + currentSelectedItem);
+            Debug.Log("Remaining gems" + _player.diamond);
+            shopPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Not enough gems");
+            shopPanel.SetActive(false);
+        }
+    }
+    // check if the slected item gem is <>= to player gem
+    // if enoguh gem then awardItem, subtract gem 
+    // or cancle sale. close shop
+
 }
---
+++ Assets/Dungeon Escape/Scripts/UI/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get 
+        {
+            if (_instance == null)
+            {
+                Debug.Log("Game Manager is null");
+            }
+            return _instance; 
+        }
+    }
+
+    public bool HasKeyToCastle { get; set; }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    
+
+
+}
